8540ef87b299451b9ca7133397b152f9cab11229
Jay Okawachi <jayokawachi@Jays-MacBook-Pro.local>
2019-01-02 09:35:10 -0800

    styled nav location drop down

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -8,7 +8,8 @@ class Nav extends React.Component {
         this.state = {
             cities: ['Alameda', 'Alhambra', 'Altadena', 'Anza', 'Arcadia', 'Bakersfield', 'Bellflower', 'Belmont', 'Big Sur', 'Calabasas', 'Canoga Park', 'Capistrano Beach', 'Carson', 'Cerritos', 'Commerse', 'Compton', 'Costa Mesa', 'Cupertino', 'Dana Point', 'Diamond Bar', 'Downey', 'Encino', 'Fairfax', 'Fremont', 'Fullerton', 'Gilroy', 'Glendora', 'Hawthorne', 'Hollywood', 'Humbolt', 'Irvine', 'Joshua Tree', 'La Canada', 'Laguna Beach', 'Lake Forest', 'Lake Tahoe', 'Malibu', 'Mendocino', 'Napa', 'Newport', 'Newport Beach', 'Oakland', 'Oxnard', 'Orange', 'Ojai', 'Palm Springs', 'Palo Alto', 'Pasadena', 'Playa del Rey', 'Pomona', 'Ramona', 'Rancho Dominguez', 'Redondo Beach', 'Rosemead', 'San Bruno', 'San Diego', 'Santa Fe Springs', 'Santa Monica', 'South Pasadena', 'Temecula', 'Topanga', 'Torrance', 'Tustin', 'Universal City', 'Van Nuys', 'Venice', 'Ventura', 'West Covina', 'West Hollywood', 'Westminister', 'Whittier', 'Woodland Hills', 'Yorba Linda', 'Yucca Valley'],
             metros: ['Atlanta/Georgia', 'Boston/New_England', 'Chicago/Illinois', 'Dallas', 'Denver/Colorado', 'Houston', 'Las_Vegas', 'Los_Angeles', 'Miami/South_Florida', 'New Orleans/Louisiana', 'New_York_Area', 'Orange_County', 'San_Francisco_Area', 'San_Diego', 'Seattle_Area'],
-            currentMetro: 'Los_Angeles'
+            currentMetro: 'Los_Angeles',
+            random: 20
         }
         this.handleSelection = this.handleSelection.bind(this);
         this.getCities = this.getCities.bind(this);
@@ -38,6 +39,12 @@ class Nav extends React.Component {
             })
             .catch(err => console.log(err))
     }
+    generateRandomNum() {
+        const min = 1;
+        const max = 4000;
+        const rand = min + Math.random() * (max - min);
+        this.setState({ random: this.state.random + rand });
+    }
 
     render() {
         return (
@@ -49,12 +56,17 @@ class Nav extends React.Component {
                 <div className={style.midLocationNav}>
                     <div className={style.leftMidLocationNav}>
                         <div className={style.regionCityList}>
-                            {this.state.metros.map(metro => <div className={style.singleRegionCityDiv}><a href onClick={this.handleSelection}> {metro} </a></div>)}
+                            {this.state.metros.map(metro => <a href className={style.singleRegionCityDiv} onClick={this.handleSelection}>
+                                <div className={style.metroNameRender}>{metro}</div>
+                                <i className={"material-icons" + ' ' + style.arrowDownLogo}>keyboard_arrow_down</i>
+                            </a>)}
                         </div>
                     </div>
                     <div className={style.rightMidLocationNav}>
                         <div className={style.regionCityList}>
-                            {this.state.cities.map(city => <div className={style.singleRegionCityDiv}><a href> {city.city} </a></div>)}
+                            {this.state.cities.map(city => <a href className={style.singleRegionCityDiv}>
+                                <div className={style.cityNameRender}>{city.city}</div> <div className={style.cityNameQuantity}>{Math.floor(Math.random() * (1000 - 1))}</div>
+                            </a>)}
                         </div>
                     </div>
                 </div>
